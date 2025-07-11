145494dacc56c540a0e1e57cfa177c75056466f5
kumar25216 <the.anubhav1404@gmail.com>
2025-07-11 22:42:30 +0530

    send_animation_screen.dart

--- lib/screens/send_animation_screen.dart
+++ lib/screens/send_animation_screen.dart
@@ -19,22 +19,38 @@ class _SendAnimationScreenState extends State<SendAnimationScreen>
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-        duration: const Duration(milliseconds: 1000), vsync: this);
 
-    _scaleAnim = Tween<double>(begin: 0.1, end: 1.2).animate(
-        CurvedAnimation(parent: _controller, curve: Curves.elasticOut))
-      ..addStatusListener((status) {
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    _scaleAnim = Tween<double>(begin: 0.1, end: 1.4).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.elasticOut),
+    )..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          Navigator.pushReplacement(
+          // Wait briefly then go to progress screen
+          Future.delayed(Duration(milliseconds: 600), () {
+            Navigator.pushReplacement(
               context,
               MaterialPageRoute(
-                  builder: (_) => TransferProgressScreen(file: widget.file)));
+                builder: (_) => TransferProgressScreen(file: widget.file),
+              ),
+            );
+          });
         }
       });
 
-    _controller.forward(); // file expands and flies
+    _controller.forward();
   }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  String get fileName => widget.file.path.split("/").last;
 
   @override
   Widget build(BuildContext context) {
@@ -44,19 +60,49 @@ class _SendAnimationScreenState extends State<SendAnimationScreen>
         child: ScaleTransition(
           scale: _scaleAnim,
           child: Container(
-            padding: EdgeInsets.all(20),
+            padding: EdgeInsets.all(24),
             decoration: BoxDecoration(
-              color: Colors.lightGreen.withOpacity(0.2),
+              gradient: LinearGradient(
+                colors: [Colors.greenAccent.withOpacity(0.1), Colors.black],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
               border: Border.all(color: Colors.greenAccent),
-              borderRadius: BorderRadius.circular(12),
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.greenAccent.withOpacity(0.4),
+                  blurRadius: 20,
+                  spreadRadius: 5,
+                )
+              ],
             ),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 Icon(Icons.send_rounded, color: Colors.greenAccent, size: 60),
                 SizedBox(height: 10),
-                Text(widget.file.path.split("/").last,
-                    style: TextStyle(color: Colors.white)),
+                Text(
+                  fileName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'monospace',
+                  ),
+                ),
+                SizedBox(height: 12),
+                Text(
+                  "Preparing to send...",
+                  style: TextStyle(
+                    color: Colors.greenAccent,
+                    fontSize: 12,
+                  ),
+                ),
+                SizedBox(height: 8),
+                CircularProgressIndicator(
+                  color: Colors.greenAccent,
+                  strokeWidth: 2.5,
+                ),
               ],
             ),
           ),
