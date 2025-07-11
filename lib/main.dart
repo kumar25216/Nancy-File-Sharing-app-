7e900323a3cac8f52397fee0e99d532505d21156
kumar25216 <the.anubhav1404@gmail.com>
2025-07-12 01:13:25 +0530

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'controllers/gesture_controller.dart';
 import 'screens/home_screen.dart';
 import 'services/permissions.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await requestPermissions(); // Ask storage/location permissions
+  await requestPermissions(); // Ask for storage/location permissions
   runApp(NancyFileShare());
 }
 
 class NancyFileShare extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Nancy File Share',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Colors.black,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.black),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black,
+    return ChangeNotifierProvider(
+      create: (_) => GestureController(),
+      child: MaterialApp(
+        title: 'Nancy File Share',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: Colors.black,
+          appBarTheme: AppBarTheme(backgroundColor: Colors.black),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+            ),
           ),
         ),
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
