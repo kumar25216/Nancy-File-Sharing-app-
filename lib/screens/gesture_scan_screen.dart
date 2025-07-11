6680ed68bae6677e8ddd1c7d20947a95fce69684
kumar25216 <the.anubhav1404@gmail.com>
2025-07-11 17:05:47 +0530

    gesture_scan_screen.dart

--- lib/screens/gesture_scan_screen.dart
+++ lib/screens/gesture_scan_screen.dart
@@ -1,33 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
+import '../controllers/gesture_controller.dart';
+import '../services/pairing_manager.dart';
 
 class GestureScanScreen extends StatefulWidget {
+  final String? incomingPayload;
+
+  GestureScanScreen({this.incomingPayload});
+
   @override
   _GestureScanScreenState createState() => _GestureScanScreenState();
 }
 
 class _GestureScanScreenState extends State<GestureScanScreen> {
   late VideoPlayerController _controller;
+  bool isFileIncoming = false;
 
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.asset("assets/videos/waiting_receive.mp4")
+
+    _controller = VideoPlayerController.asset('assets/videos/waiting_receive.mp4')
       ..initialize().then((_) {
+        setState(() {});
         _controller.setLooping(true);
         _controller.play();
-        setState(() {});
       });
+
+    // Listen for fist open after pairing data exists
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final gestureController = Provider.of<GestureController>(context, listen: false);
+
+      gestureController.addListener(() {
+        if (gestureController.isFistOpened && widget.incomingPayload != null) {
+          final data = PairingManager.parsePayload(widget.incomingPayload!);
+          if (data != null) {
+            setState(() {
+              isFileIncoming = true;
+            });
+
+            // TODO: Trigger receive transfer
+            print("Receiving: ${data['fileName']}");
+            // Navigate or show UI
+          } else {
+            print("⚠️ Invalid payload. Ignored.");
+          }
+        }
+      });
+    });
   }
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
-  }
-
-  void simulateReceive() {
-    // TODO: trigger receive animation and Wi-Fi connection
   }
 
   @override
@@ -37,16 +64,19 @@ class _GestureScanScreenState extends State<GestureScanScreen> {
       body: Stack(
         alignment: Alignment.center,
         children: [
-          _controller.value.isInitialized
-              ? AspectRatio(aspectRatio: _controller.value.aspectRatio, child: VideoPlayer(_controller))
-              : CircularProgressIndicator(),
-          Positioned(
-            bottom: 50,
-            child: ElevatedButton(
-              onPressed: simulateReceive,
-              child: Text("Simulate Fist Open"),
+          if (_controller.value.isInitialized)
+            AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
             ),
-          )
+          if (isFileIncoming)
+            Positioned(
+              bottom: 80,
+              child: Text(
+                "Receiving file...",
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
         ],
       ),
     );
